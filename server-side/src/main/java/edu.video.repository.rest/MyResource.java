bf2debc6e064c2b56868be115c96a92b23bc3fa7
Marcin Kolaczyk <marcin.kolaczyk@gmail.com>
2014-05-23 13:27:23 +0200

    Added REST method to upload file through form

--- server-side/src/main/java/edu.video.repository.rest/MyResource.java
+++ server-side/src/main/java/edu.video.repository.rest/MyResource.java
@@ -1,9 +1,11 @@
 
 package edu.video.repository.rest;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import com.sun.jersey.core.header.FormDataContentDisposition;
+import com.sun.jersey.multipart.FormDataParam;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.io.*;
 
 /** Example resource class hosted at the URI path "/myresource"
  */
@@ -19,4 +21,38 @@ public class MyResource {
     public String getIt() {
         return "Hi there!";
     }
+
+    @POST
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Path("upload")                                         //probably works for file uploaded through html form
+    private String uploadFile(
+            @FormDataParam("file") InputStream uploadedInputStream,
+            @FormDataParam("file") FormDataContentDisposition fileDetail) {
+
+        String uploadedFileLocation = "c://uploadedFiles/" + fileDetail.getFileName();
+        saveToFile(uploadedInputStream, uploadedFileLocation);
+
+
+        return "uploaded";
+    }
+
+    private void saveToFile(InputStream uploadedInputStream,
+                            String uploadedFileLocation) {
+
+        try {
+            OutputStream out = null;
+            int read = 0;
+            byte[] bytes = new byte[1024];
+
+            out = new FileOutputStream(new File(uploadedFileLocation));
+            while ((read = uploadedInputStream.read(bytes)) != -1) {
+                out.write(bytes, 0, read);
+            }
+            out.flush();
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
