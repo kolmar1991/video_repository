b25435a4e11ff50a33f310487b567ab6f49bd131
Marcin Kolaczyk <marcin.kolaczyk@gmail.com>
2014-05-23 13:16:04 +0200

    Moved dependency from server-side to client-side because client is going to connect with Fuseki server. Created tutorial how to connect to Fuseki

---
+++ client-side/src/main/java/jena/example/rdf/Tutorial.java
@@ -0,0 +1,58 @@
+package jena.example.rdf;
+
+import com.hp.hpl.jena.query.DatasetAccessor;
+import com.hp.hpl.jena.query.DatasetAccessorFactory;
+import com.hp.hpl.jena.rdf.model.*;
+import com.hp.hpl.jena.vocabulary.VCARD;
+
+public class Tutorial {
+
+    public static void main(String args[]) {
+        // some definitions
+        String personURI = "http://somewhere/JohnSmith";
+        String givenName = "John";
+        String familyName = "Smith";
+        String fullName = givenName + " " + familyName;
+
+        // create an empty model
+        Model model = ModelFactory.createDefaultModel();
+
+        // create the resource
+        //   and add the properties cascading style
+        Resource johnSmith = model.createResource(personURI)
+                .addProperty(VCARD.FN, fullName)
+                .addProperty(VCARD.N,
+                        model.createResource()
+                                .addProperty(VCARD.Given, givenName)
+                                .addProperty(VCARD.Family, familyName));
+
+        DatasetAccessor datasetAccessor = DatasetAccessorFactory.createHTTP("http://localhost:3030/ds/data");
+        datasetAccessor.putModel("dss", model);
+        model = datasetAccessor.getModel("dss");
+
+        printModel(model);
+
+    }
+
+    private static void printModel(Model model) {
+        StmtIterator iter = model.listStatements();
+
+        while (iter.hasNext()) {
+            Statement stmt = iter.nextStatement();  // get next statement
+            Resource subject = stmt.getSubject();     // get the subject
+            Property predicate = stmt.getPredicate();   // get the predicate
+            RDFNode object = stmt.getObject();      // get the object
+
+            System.out.print(subject.toString());
+            System.out.print(" " + predicate.toString() + " ");
+            if (object instanceof Resource) {
+                System.out.print(object.toString());
+            } else {
+                // object is a literal
+                System.out.print(" \"" + object.toString() + "\"");
+            }
+
+            System.out.println(" .");
+        }
+    }
+}
